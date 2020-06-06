21db1f85c1ae7099431d20e95be53562c148e648
huozhenlin <zhenlin.huo@bit-beyond.com>
2020-06-06 11:09:11 +0800

    高并发取钱问题

---
+++ 操作系统相关/多线程/demo.py
@@ -0,0 +1,51 @@
+import threading
+
+
+class Account():
+
+    def __init__(self, account_no, balance):
+        self.account_no = account_no
+        self.balance = balance
+        self.rlock = threading.RLock() # 锁对象
+
+
+    def get_balance(self):
+        """
+        查询余额操作
+        :return:
+        """
+        return self.balance
+
+    def draw(self, draw_amount):
+        self.rlock.acquire() # 加锁
+        print("加锁")
+        try:
+            if draw_amount <= self.balance:
+                print("从账号取出RMB{}".format(draw_amount))
+                self.balance = self.balance - draw_amount
+                print("余额{}".format(self.get_balance()))
+
+            else:
+                print("余额不足")
+        except Exception as e:
+            print(e.args)
+        finally:
+            self.rlock.release()
+            print("锁释放")
+
+
+ming = Account('0001',50000)
+threading.Thread(name="0", target=ming.draw, args=(800,)).start()
+threading.Thread(name="0", target=ming.draw, args=(80000,)).start()
+threading.Thread(name="0", target=ming.draw, args=(800,)).start()
+threading.Thread(name="0", target=ming.draw, args=(800,)).start()
+
+
+
+
+
+
+
+
+
+
